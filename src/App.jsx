f8b3f0e99a1fbb5cec016f4c02e62bec96a5e356
Diana Pena <dianaligiapena@gmail.com>
2023-02-24 13:51:45 +0200

    3rd iteration

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,35 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import { Routes, Route } from 'react-router-dom'
 import Home from './components/Home'
 import AllBeers from './components/AllBeers'
+import SingleBeer from './components/SingleBeer'
+import axios from 'axios'
 
 function App() {
 
+  const [beers, setBeers] = useState([])
+
+  const fetchData = async () => {
+    const response = await axios.get('https://ih-beers-api2.herokuapp.com/beers');
+
+    setBeers(response.data);
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, [])
+
+  useEffect(() => {
+    console.log(beers)
+  }, [beers])
 
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/beers" element={<AllBeers />} />
+        <Route path="/beers" element={<AllBeers beers = { beers } />} />
+        <Route path='/beers/:id' element={<SingleBeer beers = { beers } />} />
       </Routes>
     </div>
   )
--- src/components/AllBeers.jsx
+++ src/components/AllBeers.jsx
@@ -1,12 +1,30 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import NavBar from './NavBar'
 
-function AllBeers() {
+function AllBeers( { beers } ) {
+    
+
   return (
 
     <>
         <NavBar />
-        <h3>All Beers</h3>
+        <h3>All Beers List</h3>
+
+        { beers.map( (beer) => {
+            return (
+                <div key={beer._id} style={{border:"1px solid black", margin:"30px", padding:"20px"}}  >
+                    <img src={beer.image_url} alt={beer.name}  style={{width:70}}  />
+                    <h2>{beer.name}</h2>
+                    <h4>{beer.tagline}</h4>
+                    <h6>{beer.contributed_by}</h6>
+                    <Link to={`/beers/${beer._id}`}>View Beer Details</Link>
+                    
+                </div>
+            )
+        } ) }
+
+
     
     </>
   )
