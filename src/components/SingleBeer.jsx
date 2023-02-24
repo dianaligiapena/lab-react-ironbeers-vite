b7336c05d16a42c8bd825f4bf541964692e0c765
Diana Pena <dianaligiapena@gmail.com>
2023-02-24 14:15:11 +0200

    4th iteration

--- src/components/SingleBeer.jsx
+++ src/components/SingleBeer.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import { useParams } from 'react-router-dom';
+import NavBar from './NavBar';
 
-function SingleBeer() {
+function SingleBeer( {beers} ) {
+  const { beerId } = useParams();
+  console.log(beerId);
+
+  const beer = beers.find( (beer) => {
+    return beer.id === beerId;
+  } );
+
+  console.log(beer);
+
   return (
-    <div>SingleBeer</div>
+    <>
+      <NavBar />
+
+      <img src={beer.image_url} alt={beer.name}  style={{width:70}}  />
+      <h3>Name: {beer.name}</h3>
+      <h4>Tagline: {beer.tagline}</h4>
+      <h5>First brewed: {beer.first_brewed}</h5>
+      <h5>Attenuation level: {beer.attenuation_level}</h5>
+      <h5>Description: {beer.description}</h5>
+      <h5>Contributed by: {beer.contributed_by}</h5>
+
+    </>
   )
 }
 
